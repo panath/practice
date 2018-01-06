725fc28b21270bccd8cbe261090afcc86e6b3ab2
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2018-01-06 14:52:41 +0200

    adding geekforgeeks

---
+++ geekforgeeks/test_perm.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/* swap value of two pointers */
+void swap(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+/*
+   String
+   Starting index of the string
+   Ending index of the string
+*/
+void permute(char *a, int l, int r)
+{
+    int i;
+    if (l==r)
+        printf("%s\n", a);
+    else
+    {
+        for (i=l; i<=r; i++)
+        {
+            swap((a+l), (a+i));
+            permute(a, l+1, r);
+            swap((a+l), (a+i));
+        }
+    }
+}
+
+/* Driver program to test above functions */
+int main()
+{
+    char str[] = "ABC";
+    int n = strlen(str);
+    permute(str, 0, n-1);
+    return 0;
+}
