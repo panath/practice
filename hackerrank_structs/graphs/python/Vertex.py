25e068e5f66cff028ed0afe7864eb25de631f8c6
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2018-01-14 19:25:48 +0200

    modified Vertex.py

--- hackerrank_structs/graphs/python/Vertex.py
+++ hackerrank_structs/graphs/python/Vertex.py
@@ -1,21 +1,18 @@
 class Vertex:
     def __init__(self, key):
-       self.id = key
+       self.key = key
        self.connectedTo = {}
 
     def addNeighbor(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
 
     def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+        return str(self.key) + ' connectedTo: ' + str([x for x in self.connectedTo])
     def getConnections(self):
         return self.connectedTo.keys()
 
     def getId(self):
-        return self.id
-
-    def getWeight(self, nbr):
-        return self.connectedTo[nbr]
+        return self.key
 
     def getWeight(self, nbr):
         return self.connectedTo[nbr]
@@ -24,5 +21,8 @@ class Vertex:
 if __name__ == "__main__":
     v = Vertex(1)
     v.addNeighbor(3)
+    v.addNeighbor(4, 2)
     assert 1, v.getId()
     assert 1, v.getConnections()
+    print v.connectedTo
+    print v
