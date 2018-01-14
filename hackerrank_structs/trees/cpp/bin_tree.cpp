7d719f5e95e190a9037b30e15f34973aeca1d95b
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2018-01-14 22:48:46 +0200

    put method for binary search tree

---
+++ hackerrank_structs/trees/cpp/bin_tree.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <stddef.h>
+#include <exception>
+#include <cassert>
+
+struct NotFoundException : std::exception {
+
+};
+
+
+struct Node {
+   Node *left;
+   Node *right;
+   int key, val;
+
+   Node(int key, int val): left(nullptr), right(nullptr), key(key), val(val) {}
+   ~Node() {
+        delete left;
+        delete right;
+   }
+
+};
+
+
+class BinarySearchTree {
+    Node *root;
+    static Node *findMin(Node *node, Node **parent = 0) {
+        if (!node) return nullptr;
+        while (node->left) {
+            if (parent) *parent = node;
+            node = node->left;
+        }
+
+    }
+
+    static Node* findKey(Node *root, int key, Node** parent=0) {
+        if (parent) *parent = nullptr;
+        Node *curr = root;
+
+        while (curr) {
+            if (curr->key == key) {
+                return curr;
+            }
+            else if (key < curr->key) {
+                if (parent) *parent = curr;
+                curr = curr->left;
+            }
+            else {
+                if (parent) *parent = curr;
+                curr = curr->right;
+
+            }
+        }
+
+        return nullptr;
+    }
+
+
+public:
+
+
+
+    int get(const int key) {
+        Node *node = findKey(root, key);
+        if (!node)
+            throw NotFoundException();
+    }
+
+
+    return node->key;
+};
+
+int main() {
+
+}
