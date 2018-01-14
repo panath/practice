f8152a9fc5bd1defc59579d1620054d0c57be6db
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2018-01-14 20:54:24 +0200

    added integer pointers test

--- pointers/test_integer.c
+++ pointers/test_integer.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+void func(int *new_integer) {
+    new_integer = (int*)malloc(sizeof(int));
+   *new_integer = 3;
+}
+
 
 int main() {
     int b = 4;
-    int * a = &b;
-    printf("%d\n", a);
+    int *a;//= &b;
+    func(a);
+    //printf("%x\n", a);
     printf("%d\n", *a);
 }
