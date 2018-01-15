77ff10b1a6607be5e30b9323c121bff8ea9fbce7
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2018-01-15 21:13:04 +0200

    modified test_integer.c

--- pointers/test_integer.c
+++ pointers/test_integer.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 void func(int *new_integer) {
-    new_integer = (int*)malloc(sizeof(int));
    *new_integer = 3;
 }
 
 
 int main() {
     int b = 4;
-    int *a;//= &b;
+    int *a = &b;
     func(a);
     //printf("%x\n", a);
     printf("%d\n", *a);
