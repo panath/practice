e7f332167f46bb5261c0dde2847d8e9959ebcebf
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2018-02-03 20:16:11 +0200

    added fibo_easy.cpp

---
+++ crack/fibo_easy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int n);
+
+int main() {
+    int n;
+    cout << "Give fibo " << endl;
+    cin >> n;
+    cout << "fibo is " << fibo(n)  << endl; 
+
+}
+
+
+int fibo(int n) {
+
+    if (n < 0) {
+        return -1;
+    }
+    else if ((n==0) || (n==1)) {
+        return 1;
+    }
+    else if (n > 1) {
+        return fibo(n-1) + fibo(n-2);
+    }
+    else 
+        return -1;
+}
